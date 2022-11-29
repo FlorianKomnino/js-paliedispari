83337018af7af87506028e4786cd37047862cd75
FlorianKomnino <florian.komnino@outlook.it>
2022-11-29 15:23:33 +0100

    working on js structure setting the trim of the string's spaces and punctuation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 console.log('Hello World!');
 
+let exampleString = 'I topi, non avevano nipoti';
+
+console.log(exampleString)
+
+
+// per togliere tutti gli spazi vuoti
+exampleStringTrimmed = exampleString.trim();
+exampleStringTrimmed = exampleStringTrimmed.replace(/\s/g, '')
+console.log('dopo il trim degli spazi vuoti')
+console.log(exampleStringTrimmed)
+
+
+
+// per togliere tutta la punteggiatura
+const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+exampleString = exampleString.replace(regex, '');
+console.log(exampleString)
+
+// 
