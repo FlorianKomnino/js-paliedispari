5b213261beb528baa0a755f900724c2fb2f88c08
FlorianKomnino <florian.komnino@outlook.it>
2022-11-29 15:46:14 +0100

    added for cicle to create an array with the single letters, got from the string, in reverse order

--- js/script.js
+++ js/script.js
@@ -2,26 +2,35 @@ console.log('Hello World!');
 
 let exampleString = 'I topi, non avevano nipoti';
 
-console.log(exampleString)
+console.log(exampleString);
+
 
 
 // per togliere tutti gli spazi vuoti
+
 exampleStringTrimmed = exampleString.trim();
-exampleStringTrimmed = exampleStringTrimmed.replace(/\s/g, '')
-console.log('dopo il trim degli spazi vuoti')
-console.log(exampleStringTrimmed)
-
-
+exampleStringTrimmed = exampleStringTrimmed.replace(/\s/g, '');
+console.log('dopo il trim degli spazi vuoti');
+console.log(exampleStringTrimmed);
 
 // per togliere tutta la punteggiatura
 
-
 const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 exampleStringTrimmed = exampleStringTrimmed.replace(regex, '');
-console.log(exampleStringTrimmed)
+console.log('dopo il trim della punteggiatura');
+console.log(exampleStringTrimmed);
 
 // per rendere tutte le lettere minuscole
 
-exampleStringTrimmedAndLowed = exampleStringTrimmed
-exampleStringTrimmedAndLowed = exampleStringTrimmedAndLowed.toLowerCase()
-console.log(exampleStringTrimmedAndLowed)
+exampleStringTrimmedAndLowed = exampleStringTrimmed;
+exampleStringTrimmedAndLowed = exampleStringTrimmedAndLowed.toLowerCase();
+console.log('dopo aver reso tutte le lettere minuscole');
+console.log(exampleStringTrimmedAndLowed);
+
+let lettersArray = [];
+
+for ( let i = (exampleStringTrimmedAndLowed.length - 1 ) ; i > -1 ; i--) {
+    lettersArray.push(exampleStringTrimmedAndLowed.charAt(i));
+}
+
+console.log(lettersArray);
