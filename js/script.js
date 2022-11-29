30960bd5b053f7fcd9d8ca2c056deb9e2a2f1ece
FlorianKomnino <florian.komnino@outlook.it>
2022-11-29 15:27:28 +0100

    added .toLowerCase()

--- js/script.js
+++ js/script.js
@@ -14,8 +14,14 @@ console.log(exampleStringTrimmed)
 
 
 // per togliere tutta la punteggiatura
-const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
-exampleString = exampleString.replace(regex, '');
-console.log(exampleString)
 
-// 
+
+const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+exampleStringTrimmed = exampleStringTrimmed.replace(regex, '');
+console.log(exampleStringTrimmed)
+
+// per rendere tutte le lettere minuscole
+
+exampleStringTrimmedAndLowed = exampleStringTrimmed
+exampleStringTrimmedAndLowed = exampleStringTrimmedAndLowed.toLowerCase()
+console.log(exampleStringTrimmedAndLowed)
